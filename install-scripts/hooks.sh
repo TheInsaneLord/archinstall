050340b5fb13be3f3f0b4faac85b334dc2bba1df
TheInsaneLord <39261682+TheInsaneLord@users.noreply.github.com>
2025-05-19 23:47:36 +0100

    Update hooks.sh

--- install-scripts/hooks.sh
+++ install-scripts/hooks.sh
@@ -4,7 +4,7 @@ set -e  # Exit script on error
 
 echo "Setting up Pacman hooks..."
 
-sudo -p /etc/pacman.d/hooks
+sudo mkdir -p /etc/pacman.d/hooks
 sudo cp -v hooks/*.hook /etc/pacman.d/hooks
 
 echo "Pacman hooks installed successfully."
