c2d84578e387857bffa6e61bd1b5207c09a55722
TheInsaneLord <39261682+TheInsaneLord@users.noreply.github.com>
2025-07-27 22:31:51 +0100

    Update boot.sh

--- install-scripts/boot.sh
+++ install-scripts/boot.sh
@@ -4,27 +4,27 @@
 # if $nvidia_gpu; then
 #   echo "Loading NVIDIA prerequisites…"
 #   pacman -S --noconfirm nvidia nvidia-utils
-#   # or add the kernel param in grub…
+#   # or add the kernel param in grub… 
 # fi
 
 # Set up bootloader
 bootctl install
 touch /boot/loader/entries/def.conf
 
-# def.conf
+# def.conf contents
 echo "title Arch Linux" >> /boot/loader/entries/def.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/def.conf
 echo "initrd /amd-ucode.img" >> /boot/loader/entries/def.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/def.conf
 
 lsblk
-read -p "select a disk (sda1): " disk
+read -p "select a disk (e.g. sda1): " disk
 
 # Add boot drive to def.conf
 echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/$disk) rw nvidia-drm.modeset=1" >> /boot/loader/entries/def.conf
 
-# GRUB
+# GRUB setup
 pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
-nano /etc/default/grub # add GRUB_CMDLINE_LINUX="nvidia-drm.modeset=1"
+nano /etc/default/grub  # Add GRUB_CMDLINE_LINUX="nvidia-drm.modeset=1"
 grub-install --target=x86_64-efi --bootloader-id=grub_efi --efi-directory=/boot/ --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
