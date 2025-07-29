5e74067e183ea34043036aa21ad4744d0722361b
TheInsaneLord <39261682+TheInsaneLord@users.noreply.github.com>
2025-07-29 06:41:17 +0100

    Update apps.sh

--- install-scripts/apps.sh
+++ install-scripts/apps.sh
@@ -10,6 +10,15 @@ sudo cp -v confs/pacman.conf /etc/pacman.conf
 echo "Refreshing all package databases (including multilib)…"
 pacman -Syy --noconfirm
 
+# install nvidia drivers
+read -p "install prprietery drivers for nvidia? (Y/n) " user_choic
+if [[ "$user_choice" =~ ^[Yy]$ ]] || [[ -z "$user_choice" ]]; then
+    sudo pacman -S nvidia-dkms nvidia-utils opencl-nvidia libglvnd lib32-libglvnd lib32-nvidia-utils lib32-opencl-nvidia nvidia-settings
+    sudo cp -v confs/mkinitcpio.conf /etc/mkinitcpio.conf # copy mkinitcpio.conf
+else
+    echo 'Skippinf Nvidia Drivers'
+fi
+
 # Install core applications
 main_apps=(konsole firefox steam kate dolphin fastfetch git bash-completion flatpak bashtop pacman-contrib man ufw)
 
