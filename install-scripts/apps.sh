860bc1c9049a0ec313839981a14ae61bf7862ffc
TheInsaneLord <39261682+TheInsaneLord@users.noreply.github.com>
2025-07-29 07:04:53 +0100

    Update apps.sh

--- install-scripts/apps.sh
+++ install-scripts/apps.sh
@@ -49,7 +49,7 @@ echo "sudo usermod -aG lp <username>"
 
 # Audio setup for Pipewire
 sudo pacman -S --noconfirm pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber qjackctl
-systemctl enable --now pipewire pipewire.socket wireplumber
+#systemctl enable --now pipewire pipewire.socket wireplumber
 
 # Prompt for Flatpak installation
 read -p "Do you want to install Flatpaks? (Y/n) " flatpak_choice
