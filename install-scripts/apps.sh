d9b485d5e0e9e5dd5c78ea000827eabe52dde30c
TheInsaneLord <39261682+TheInsaneLord@users.noreply.github.com>
2025-08-06 07:54:04 +0100

    Update apps.sh

--- install-scripts/apps.sh
+++ install-scripts/apps.sh
@@ -28,7 +28,7 @@ else
 fi
 
 # Install optional apps
-opt_apps=(vlc traceroute libreoffice-fresh p7zip python3 python-pip filezilla unison filezilla openrgb)
+opt_apps=(vlc traceroute libreoffice-fresh p7zip python3 python-pip filezilla unison filezilla openrgb jdk-openjdk)
 
 read -p "Do you want to install optional apps? (Y/n) " opt_apps_choice
 if [[ "$opt_apps_choice" =~ ^[Yy]$ ]] || [[ -z "$opt_apps_choice" ]]; then
